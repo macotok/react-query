88bb3705a94f2478cf04cb8c58b96def62e94c86
macotok <macotock@gmail.com>
2022-12-11 12:00:44 +0900

    setup and router with react app

---
+++ react-query-demo/src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+
+import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+
+import { HomePage } from './components/Home.page';
+import { RQSuperHeroesPage } from './components/RQSuperHeroes.page';
+import { SuperHeroesPage } from './components/SuperHeroes.page';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/super-heroes">Traditional Super Heroes</Link>
+            </li>
+            <li>
+              <Link to="/rq-super-heroes">RQ Super Heroes</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/super-heroes" element={<SuperHeroesPage />} />
+          <Route path="/rq-super-heroes" element={<RQSuperHeroesPage />} />
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
