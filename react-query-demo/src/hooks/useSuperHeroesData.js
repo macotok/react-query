1d0206737f66a9397cf4b723286b8b5eee4dda8c
macotok <macotock@gmail.com>
2022-12-25 16:12:03 +0900

    Axios interceptor

--- react-query-demo/src/hooks/useSuperHeroesData.js
+++ react-query-demo/src/hooks/useSuperHeroesData.js
@@ -1,9 +1,9 @@
 import { useMutation, useQuery, useQueryClient } from 'react-query';
 
-import axios from 'axios';
+import { request } from '../utils/axios-utils';
 
 const fetcher = () => {
-  return axios.get('http://localhost:4000/superheroes');
+  return request({ url: '/superheroes' });
 };
 
 export const useSuperHeroesData = (onSuccess, onError) => {
@@ -14,7 +14,7 @@ export const useSuperHeroesData = (onSuccess, onError) => {
 };
 
 const addSuperHero = (hero) => {
-  return axios.post('http://localhost:4000/superheroes', hero);
+  return request({ url: '/superheroes', method: 'post', data: hero });
 };
 
 export const useAddSuperHeroData = () => {
